1ff3aac556865eb9100308a142dbe782285a1e10
Silvio Donato <silvio.donato@cern.ch>
2021-11-03 12:30:28 +0100

    remove sleep

--- Run3/downloadCustomizationFunctions.sh
+++ Run3/downloadCustomizationFunctions.sh
@@ -1,13 +1,8 @@
 curl -O https://raw.githubusercontent.com/SWuchterl/RecoBTag-PerformanceMeasurements/Run3_ForJIRA/python/customise_TRK.py
-sleep 0.1
 curl -O https://raw.githubusercontent.com/SWuchterl/RecoBTag-PerformanceMeasurements/Run3_ForJIRA/python/customise_TRK_replacement_global_calo.py
-sleep 0.1
 curl -O https://raw.githubusercontent.com/mmasciov/cmssw/defaultRun3Tracking_forJIRA/HLTrigger/Configuration/python/customizeHLTforRun3Tracking.py
-sleep 0.1
 curl -O https://raw.githubusercontent.com/annamasce/TauTriggerTools/triggerRnD_counter/HLTProducers/python/applyL2TauTag.py
-sleep 0.1
 curl -O https://raw.githubusercontent.com/khaosmos93/MuonHLTForRun3/master/customizeMuonHLTForRun3.py
-sleep 0.1
 
 #Fix for CMSSW_12_1_0_pre3 (#33885)
 
